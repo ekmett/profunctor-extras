18fff76ccbddc2e756f04833ba8b68a716fdaaff
Edward Kmett <ekmett@gmail.com>
2013-01-02 22:41:44 -0500

    Convered to type-families

--- src/Data/Profunctor/Rep.hs
+++ src/Data/Profunctor/Rep.hs
@@ -17,9 +17,9 @@
 module Data.Profunctor.Rep
   (
   -- * Representable Profunctors
-    Rep(..), tabulated
+    Representable(..), tabulated
   -- * Corepresentable Profunctors
-  , Corep(..), cotabulated
+  , Corepresentable(..), cotabulated
   ) where
 
 import Control.Arrow
@@ -35,24 +35,29 @@ import Data.Tagged
 
 -- | A 'Profunctor' @p@ is representable if there exists a 'Functor' @f@ such that
 -- @p d c@ is isomorphic to @d -> f c@.
-class (Functor f, Profunctor p) => Rep f p where
-  tabulate :: (d -> f c) -> p d c
-  rep :: p d c -> d -> f c
+class (Functor (Rep p), Profunctor p) => Representable p where
+  type Rep p :: * -> *
+  tabulate :: (d -> Rep p c) -> p d c
+  rep :: p d c -> d -> Rep p c
 
-instance Rep Identity (->) where
+instance Representable (->) where
+  type Rep (->) = Identity
   tabulate f = runIdentity . f
   rep f = Identity . f
 
-instance (Monad m, Functor m) => Rep m (Kleisli m) where
+instance (Monad m, Functor m) => Representable (Kleisli m) where
+  type Rep (Kleisli m) = m
   tabulate = Kleisli
   rep = runKleisli
 
-instance Functor f => Rep f (UpStar f) where
+instance Functor f => Representable (UpStar f) where
+  type Rep (UpStar f) = f
   tabulate = UpStar
   rep = runUpStar
 
 -- | The composition of two representable profunctors is representable by the composition of their representations.
-instance (Rep f p, Rep g q) => Rep (Compose f g) (Procompose p q) where
+instance (Representable p, Representable q) => Representable (Procompose p q) where
+  type Rep (Procompose p q) = Compose (Rep p) (Rep q)
   tabulate f = Procompose (tabulate (getCompose . f)) (tabulate id)
   rep (Procompose f g) d = Compose $ rep g <$> rep f d
 
@@ -60,37 +65,43 @@ instance (Rep f p, Rep g q) => Rep (Compose f g) (Procompose p q) where
 --
 -- This can be used with the combinators from the @lens@ package.
 --
--- @'tabulated' :: 'Rep' f p => 'Iso'' (d -> f c) (p d c)@
-tabulated :: (Profunctor r, Functor h, Rep f p, Rep g q)
-          => r (p d c) (h (q d' c'))
-          -> r (d -> f c) (h (d' -> g c'))
+-- @'tabulated' :: 'Representable' p => 'Iso'' (d -> 'Rep' p c) (p d c)@
+tabulated :: (Profunctor r, Functor f, Representable p, Representable q)
+          => r (p d c) (f (q d' c'))
+          -> r (d -> Rep p c) (f (d' -> Rep q c'))
 tabulated = dimap tabulate (fmap rep)
 
 -- * Corepresentable Profunctors
 
--- | A 'Profunctor' @p@ is representable if there exists a 'Functor' @f@ such that
--- @p d c@ is isomorphic to @d -> f c@.
-class (Functor f, Profunctor p) => Corep f p where
-  cotabulate :: (f d -> c) -> p d c
-  corep :: p d c -> f d -> c
+-- | A 'Profunctor' @p@ is corepresentable if there exists a 'Functor' @f@ such that
+-- @p d c@ is isomorphic to @f d -> c@.
+class (Functor (Corep p), Profunctor p) => Corepresentable p where
+  type Corep p :: * -> *
+  cotabulate :: (Corep p d -> c) -> p d c
+  corep :: p d c -> Corep p d -> c
 
-instance Corep Identity (->) where
+instance Corepresentable (->) where
+  type Corep (->) = Identity
   cotabulate f = f . Identity
   corep f (Identity d) = f d
 
-instance Functor w => Corep w (Cokleisli w) where
+instance Functor w => Corepresentable (Cokleisli w) where
+  type Corep (Cokleisli w) = w
   cotabulate = Cokleisli
   corep = runCokleisli
 
-instance Corep Proxy Tagged where
+instance Corepresentable Tagged where
+  type Corep Tagged = Proxy
   cotabulate f = Tagged (f Proxy)
   corep (Tagged a) _ = a
 
-instance Functor f => Corep f (DownStar f) where
+instance Functor f => Corepresentable (DownStar f) where
+  type Corep (DownStar f) = f
   cotabulate = DownStar
   corep = runDownStar
 
-instance (Corep f p, Corep g q) => Corep (Compose g f) (Procompose p q) where
+instance (Corepresentable p, Corepresentable q) => Corepresentable (Procompose p q) where
+  type Corep (Procompose p q) = Compose (Corep q) (Corep p)
   cotabulate f = Procompose (cotabulate id) (cotabulate (f . Compose))
   corep (Procompose f g) (Compose d) = corep g $ corep f <$> d
 
@@ -99,7 +110,7 @@ instance (Corep f p, Corep g q) => Corep (Compose g f) (Procompose p q) where
 -- This can be used with the combinators from the @lens@ package.
 --
 -- @'tabulated' :: 'Corep' f p => 'Iso'' (f d -> c) (p d c)@
-cotabulated :: (Profunctor r, Functor h, Corep f p, Corep g q)
+cotabulated :: (Profunctor r, Functor h, Corepresentable p, Corepresentable q)
           => r (p d c) (h (q d' c'))
-          -> r (f d -> c) (h (g d' -> c'))
+          -> r (Corep p d -> c) (h (Corep q d' -> c'))
 cotabulated = dimap cotabulate (fmap corep)
